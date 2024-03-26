26e13b14d6e4d44cdd0ee5528cf2131dcb985c45
denblackson <denblackson@gmail.com>
2024-03-26 21:16:04 +0200

    Add project files.

---
+++ BookStore.Domain/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿
+namespace BookStore.Domain.Models
+{
+    public class Book
+    {
+        public Guid Id { get; }
+        public string Title { get; } = string.Empty;
+        public string Description { get; } = string.Empty;
+        public decimal Price { get; }
+
+        private Book(Guid id, string title, string description, decimal price)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Price = price;
+        }
+
+        public static (Book book, string errorMsg) Create (Guid id, string title, string description, decimal price)
+    }
+}
